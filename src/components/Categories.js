4bcfd9bb8bf1d364fc9f8b8aa0e1b0d87759cfa5
amare1990 <amaremek@gmail.com>
2022-11-11 00:04:42 +0300

    Add Categories component with only one button

---
+++ src/components/Categories.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Categories extends React.Component {
+  constructor(props) {
+    this.props = props;
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+      <h1> Under Construction</h1>
+      </>
+    );
+  }
+}
+
+export default Categories;
