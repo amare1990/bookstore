cd6674a21e5587da5763aced0a2297caf2842b62
amare1990 <amaremek@gmail.com>
2022-11-14 22:38:40 +0300

    Install Redux toolkit

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -4,7 +4,7 @@ const InputBook = () => (
   <form className="input-book">
     <input type="text" className="book-title input-field" placeholder="Book title" />
     <input type="text" className="author input-field" placeholder="author" />
-    <button type="button"> Check status </button>
+    <button type="button"> ADD BOOK </button>
   </form>
 );
 
