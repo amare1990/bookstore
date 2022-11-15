bd2bab95dca144115bb561a5a98dcf510a93624e
amare1990 <amaremek@gmail.com>
2022-11-16 01:50:47 +0300

    Wrap my application with Provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
+// import { Provider } from 'react-redux';
 import Books from './components/Books';
 import Categories from './components/Categories';
 import './App.css';
+// import store from './redux/configureStore';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 // import { configureStore } from '@reduxjs/toolkit';
-// import { Provider } from 'react-redux';
-// import rootReducers from '../reducers';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 // import { createStore } from 'redux';
 import './index.css';
 import App from './App';
@@ -14,7 +14,9 @@ root.render(
   // <Provider store={store}>
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   // </Provider>,
