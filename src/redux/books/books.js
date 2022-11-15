e9617b576d26e0a587266aae1c00360cc4b03760
amare1990 <amaremek@gmail.com>
2022-11-15 13:31:17 +0300

    Add actioncreators and reducers for the books

---
+++ src/redux/books/books.js
@@ -0,0 +1,38 @@
+// Constants
+const ADD_BOOK = 'bookstore/src/redux/books/addBook';
+const REMOVE_BOOK = 'bookstore/src/redux/books/removeBOOK';
+
+// reducers
+export default function booksReducer(state = [], action) {
+  switch (action.type) {
+    case ADD_BOOK :
+      return [
+        ...state,
+        {
+          id: action.id,
+          title: action.title,
+          author: action.author,
+        },
+      ];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.book.id);
+    default:
+      return state;
+  }
+}
+
+// Action creators
+
+export const addBook = () => {
+  return {
+    type: ADD_BOOK,
+    book: {id: 0, title: 'Operating Systems', author: 'xxx'}
+  };
+};
+
+export const removeBook = () => {
+  return {
+    type: REMOVE_BOOK,
+    payload: 'Removed'
+  };
+};
