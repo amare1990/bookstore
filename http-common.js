294e647ca46c592e1ed39ef0e460463c8247b06a
amare1990 <amaremek@gmail.com>
2022-11-17 21:43:18 +0300

    Update the API url

--- http-common.js
+++ http-common.js
@@ -1,3 +1,8 @@
+// APPID created using
+/* Generating app-id for  curl -X POST -d 'name=Amare'
+https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/
+And was found to be SSn5lljqiXcoSQyQzIb4 */
+
 const APP_ID = 'SSn5lljqiXcoSQyQzIb4';
 const BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/';
 const FETCH_BOOK_URL = `${BASE_URL}/${APP_ID}/books`;
